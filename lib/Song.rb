2f1e03568dd79a55fe38a465dce747d9480e3a6a
Melissa Sperry <sperry.mel@gmail.com>
2020-06-12 02:27:56 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -1,86 +1,22 @@
-require 'pry'
 
-class Song
-  attr_accessor :name, :artist, :genre, :musicimporter, :musiclibrarycontroller
-  extend Concerns::Findable
+class MusicImporter
 
-  @@all = []
+  attr_reader :path
 
-  def initialize(name, artist=nil, genre=nil)
-    @name = name
-    self.artist=(artist) if artist != nil
-    self.genre=(genre) if genre != nil
+  def initialize(path)
+    @path = path
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(song)
-    song = self.new(song)
-    song.save
-    song
-  end
-
-  def artist
-    @artist
-  end
-
-  def artist=(artist)
-    @artist = artist
-    artist.add_song(self)
-  end
-
-  def genre
-    @genre
-  end
-
-  def genre=(genre)
-    @genre = genre
-    genre.songs << self unless genre.songs.include?(self)
-  end
-
-  def self.find_by_name(name)
-    @@all.detect do |song|
-      song.name == name
+#returns all imported file names
+  def files
+    files = []
+    Dir.glob("#{path}/*.mp3").collect do |file|
+      files << file.gsub("#{path}/", "")
     end
+    files
   end
 
-  def self.find_or_create_by_name(name)
-    # @@all.detect do |song|
-    #   if song.name == name
-    #     song
-    #   else
-    #     self.create(name)
-    #   end
-    # end
-    self.find_by_name(name) || self.create(name)
-
+  def import
+    files.each{|file| Song.new_by_}
   end
-
-  def self.new_from_filename(filename)
-    array = filename.split(" - ")
-
-    song_name = array[1]
-    artist_name = array[0]
-    genre_name = array[2].split(".mp3").join
-
-    artist = Artist.find_or_create_by_name(artist_name)
-    genre = Genre.find_or_create_by_name(genre_name)
-    self.new(song_name, artist, genre)
-  end
-
-  def self.create_from_filename(filename)
-    self.new_from_filename(filename).save
-  end
-
-
 end
